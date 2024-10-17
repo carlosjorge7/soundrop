f3a96578fab5c54dfdcbc2fc56d70aece7fdd70f
Carlos <carlosjorgech7@gmail.com>
2024-10-17 14:10:44 +0200

    add menu

--- src/app/private/library/library.component.ts
+++ src/app/private/library/library.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  IonContent,
+  IonCard,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.scss'],
   standalone: true,
+  imports: [
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonCard,
+    IonContent,
+    RouterLink,
+  ],
 })
-export class LibraryComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
-}
+export class LibraryComponent {}
--- src/app/private/spotify/services/spotify.service.ts
+++ src/app/private/spotify/services/spotify.service.ts
@@ -7,19 +7,25 @@ import { ResponseSpoty } from '../models/spotify';
   providedIn: 'root',
 })
 export class SpotifyService {
-  private apiUrlSpoty =
-    'https://spotify-downloader9.p.rapidapi.com/downloadSong';
+  private apiUrlSpoty = 'https://spotify-downloader9.p.rapidapi.com';
+
   private apiKeySpoty = 'cff3589cb7msh4356e5ee7bf14cdp1a2aecjsne8f75aee4620';
   private apiHostSpoty = 'spotify-downloader9.p.rapidapi.com';
+
+  private headers = new HttpHeaders({
+    'x-rapidapi-key': this.apiKeySpoty,
+    'x-rapidapi-host': this.apiHostSpoty,
+  });
 
   constructor(private http: HttpClient) {}
 
   public downloadSpoty(songId: string): Observable<ResponseSpoty> {
-    const headers = new HttpHeaders({
-      'x-rapidapi-key': this.apiKeySpoty,
-      'x-rapidapi-host': this.apiHostSpoty,
-    });
-    const url = `${this.apiUrlSpoty}?songId=${songId}`;
-    return this.http.get<ResponseSpoty>(url, { headers });
+    const url = `${this.apiUrlSpoty}/downloadSong?songId=${songId}`;
+    return this.http.get<ResponseSpoty>(url, { headers: this.headers });
+  }
+
+  public downloadPlaylist(playlistId: string): Observable<any> {
+    const url = `${this.apiUrlSpoty}/downloadPlaylist?playlistId=${playlistId}`;
+    return this.http.get(url, { headers: this.headers });
   }
 }
